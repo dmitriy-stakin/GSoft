b9ec6b2d066eda0e06c7f1836fb4a20391185c1c
dmitriy-stakin <mailofme@mail.ru>
2023-12-11 19:30:07 +0700

    build art

---
+++ modulka landing/v3/cases/bao/case-scripts.js
@@ -0,0 +1,10 @@
+var leaves = document.querySelectorAll('.leaves')
+
+if(!isMobile) {
+    window.addEventListener('scroll', () => {
+        let { scrollY } = window;
+        leaves.forEach(leave => {
+                leave.style.translate = '0 ' + -0.2 * scrollY + 'px';
+        });
+    }) 
+}
